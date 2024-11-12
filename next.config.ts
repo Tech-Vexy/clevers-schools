f749fd7fc9fda9aeddc1177ad5d164707ea29704
Veldrine Evelia Kaharwa <eveliaveldrine@outlook.com>
2024-11-12 08:28:53 +0000

    optimize code

--- next.config.ts
+++ next.config.ts
@@ -3,15 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   compress: true,
-  // Additional compression options
-  webpack: (config, { dev, isServer }) => {
-    if (!dev && !isServer) {
-      config.optimization.splitChunks = {
-        chunks: 'all',
-        minSize: 20000,
-        maxSize: 244000
-      }
-    }
+
 };
 
 export default nextConfig;
