15d17012e8cb50ef2424f1a449aeb09f8d53a3a6
Veldrine Evelia Kaharwa <eveliaveldrine@outlook.com>
2025-03-22 09:14:54 +0300

    update config

--- app/api/user/password/route.ts
+++ app/api/user/password/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest, NextResponse } from "next/server"
 import { getServerSession } from "next-auth"
 import { authOptions } from "@/auth"
-import { connectToDatabase } from "@/lib/db"
+import { connectToDatabase } from "@/lib/mongodb"
 import { ObjectId } from "mongodb"
 import bcrypt from "bcrypt"
 
